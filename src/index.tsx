be65119b7de0264a1d3ed4675a6e8639a9935239
ihorDudkevych <egorka01051993@gmail.com>
2023-02-23 22:22:35 +0200

    rework

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { Provider } from 'react-redux';
 import App from './Components/App';
+
 import store from './store/store';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById('root')
 );
